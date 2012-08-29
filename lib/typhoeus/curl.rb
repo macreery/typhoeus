1fbdd29ff6d86dd051eb937fd36cee11e78db34d
Frank Macreery <frank@macreery.com>
2012-08-29 17:45:25 -0400

    Load either libcurl.so or libcurl.so.4 for Debian/Ubuntu support

--- lib/typhoeus/curl.rb
+++ lib/typhoeus/curl.rb
@@ -402,7 +402,7 @@ module Typhoeus
     callback :callback, [:pointer, :size_t, :size_t, :pointer], :size_t
 
     ffi_lib_flags :now, :global
-    ffi_lib 'libcurl'
+    ffi_lib ['libcurl', 'libcurl.so.4']
 
     attach_function :global_init, :curl_global_init, [:long], :int
 
